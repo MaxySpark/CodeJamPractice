ea70f7e8c13b2d643d28a2ae42f129b517038329
MaxySpark <me@bhargab.me>
2017-03-15 22:53:38 +0530

    Complete C. T9 Spelling

--- Africa 2010 - Qualification Round/C. T9 Spelling/code_3.cpp
+++ Africa 2010 - Qualification Round/C. T9 Spelling/code_3.cpp
@@ -14,32 +14,66 @@ const int P[4] = { 7777, 777, 77, 7 };
 const int T[3] = { 888, 88 ,8 };
 const int W[4] = { 9999 ,999, 99, 9 };
 
+int s = 0;
+
 int getPos(int x) {
     int n;
     if(x>=97 && x<=99) {
         n=99-x;
+        if(s==2) {
+            cout<<" ";
+        }
         cout<<A[n];
+        s=2;
     } else if( x>=100 && x<=102) {
         n=102-x;
+        if(s==3) {
+            cout<<" ";
+        }
         cout<<D[n];
+        s=3;
     } else if( x>=103 && x<=105) {
         n=105-x;
+        if(s==4) {
+            cout<<" ";
+        }
         cout<<G[n];
+        s=4;
     } else if( x>=106 && x<=108) {
         n=108-x;
+        if(s==5) {
+            cout<<" ";
+        }
         cout<<J[n];
+        s=5;
     } else if( x>=109 && x<=111) {
         n=111-x;
+        if(s==6) {
+            cout<<" ";
+        }
         cout<<M[n];
+        s=6;
     } else if( x>=112 && x<=115) {
         n=115-x;
+        if(s==7) {
+            cout<<" ";
+        }
         cout<<P[n];
+        s=7;
     } else if( x>=116 && x<=118) {
         n=118-x;
+        if(s==8) {
+            cout<<" ";
+        }
         cout<<T[n];
+        s=8;
     } else if( x>=119 && x<=122) {
         n=122-x;
+        if(s==9) {
+            cout<<" ";
+        }
         cout<<W[n];
+        s=9;
     } else if(x==32) {
         cout<<"0";
     }
@@ -68,6 +102,7 @@ int main()
             getPos(int(WordList[j][i]));
         }
 	 cout<<endl;
+	 s=0;
     }
 
 
